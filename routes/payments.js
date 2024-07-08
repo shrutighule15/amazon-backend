b75bbe950d42840c01ff4ff7ea6aaaa37d3c49ea
shruti <shrutighule15@gmail.com>
2024-07-08 22:49:46 +0530

    backend

--- routes/payments.js
+++ routes/payments.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const router = express.Router()
-const razorpay = require('razorpay')
+const express = require("express");
+const router = express.Router();
+const razorpay = require("../models/payment");
 
 //Route to create an order
-router.post('/order',async(req, res)=> {
-    const{amount, currency, receipt} = req.body;
-    const options ={
-        amount: amount * 100,
-        currency,
-        receipt
-    }
+router.post("/order", async (req, res) => {
+  const { amount, currency, receipt } = req.body;
+  const options = {
+    amount: amount * 100,
+    currency,
+    receipt,
+  };
 
-    try{
-        const order = await razorpay.orders.create(options)
-        res.json(order);
-    }
-catch (error){
-    res.status(500).send(error)
-}
-})
+  try {
+    const order = await razorpay.orders.create(options);
+    res.json(order);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(error);
+  }
+});
 module.exports = router;
